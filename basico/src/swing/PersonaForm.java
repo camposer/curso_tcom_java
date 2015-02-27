ff4d7d2ed60b224abf7a38aa6a7b61290897632b
Rodolfo Campos <camposer@gmail.com>
2015-02-27 14:16:07 +0100

    Ejemplo básico de swing (menubar)

--- basico/src/swing/PersonaForm.java
+++ basico/src/swing/PersonaForm.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
@@ -15,30 +16,30 @@ import javax.swing.JTextField;
 
 public class PersonaForm extends JFrame {
 	private JPanel panel;
-	
+
 	private JLabel nombreLabel;
 	private JTextField nombreText;
-	
+
 	private JTextField edadText;
 	private JLabel edadLabel;
-	
+
 	private JTextField alturaText;
 	private JLabel alturaLabel;
-	
+
 	private JButton aceptarBtn;
-	
+
 	public PersonaForm() {
 		panel = new JPanel();
-		
+
 		nombreLabel = new JLabel("Nombre: ");
 		nombreText = new JTextField();
 
 		edadLabel = new JLabel("Edad: ");
 		edadText = new JTextField();
-		
+
 		alturaLabel = new JLabel("Altura: ");
 		alturaText = new JTextField();
-		
+
 		aceptarBtn = new JButton("Aceptar");
 		aceptarBtn.addActionListener(new ActionListener() {
 			@Override
@@ -46,15 +47,15 @@ public class PersonaForm extends JFrame {
 				String nombre = nombreText.getText();
 				float altura = Float.parseFloat(alturaText.getText());
 				int edad = Integer.parseInt(edadText.getText());
-				
+
 				System.out.println(new Persona(nombre, altura, edad));
 			}
 		});
 	}
-	
+
 	public void iniciar() {
 		JMenuBar menu = new JMenuBar();
-		JMenuItem ayuda = new JMenuItem("Ayuda");
+		JMenu ayuda = new JMenu("Ayuda");
 		JMenuItem cerrar = new JMenuItem("Cerrar");
 		cerrar.addActionListener(new ActionListener() {
 			@Override
@@ -62,20 +63,20 @@ public class PersonaForm extends JFrame {
 				System.exit(0);
 			}
 		});
-		
+
 		ayuda.add(cerrar);
 		menu.add(ayuda);
 		setJMenuBar(menu);
 
 		panel.setLayout(new GridLayout(3, 2));
-		
+
 		panel.add(nombreLabel);
 		panel.add(nombreText);
 		panel.add(edadLabel);
 		panel.add(edadText);
 		panel.add(alturaLabel);
 		panel.add(alturaText);
-		
+
 		this.add(panel, BorderLayout.CENTER);
 		this.add(aceptarBtn, BorderLayout.SOUTH);
 
@@ -83,11 +84,8 @@ public class PersonaForm extends JFrame {
 		this.setSize(200, 200);
 		this.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		new PersonaForm().iniciar();
 	}
 }
-
-
-
